f6fcd9db2cd375d7c09fb09b13f976f333bebe3d
borisaxel <boris.ebaneth@ossas-it.com>
2023-06-20 21:05:44 +0100

    Add base_path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BASE_PATH: 'http://localhost:11550/expenses',
+  // BASE_PATH: '',
 };
 
 /*
